423ca83a441d35eac883800cfe85129527420880
mingang.he <dustgle@gmail.com>
2022-07-02 23:32:10 +0800

    Update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -49,4 +49,31 @@ return require('packer').startup(function()
     'nvim-treesitter/nvim-treesitter-textobjects'
   }
 
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      { 'hrsh7th/cmp-buffer', after = 'nvim-cmp', },
+      { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp', },
+      { 'hrsh7th/cmp-path', after = 'nvim-cmp', },
+      { 'saadparwaiz1/cmp_luasnip', after = 'LuaSnip', },
+    },
+    config = function() require('config.cmp') end,
+  }
+
+  use 'L3MON4D3/LuaSnip'
+  use 'rafamadriz/friendly-snippets'
+
+  use 'williamboman/nvim-lsp-installer'
+
+  use {
+    'neovim/nvim-lspconfig',
+    config = function()
+      require('lsp')
+      vim.defer_fn(function()
+        vim.cmd("silent! e%")
+      end, 0)
+    end,
+    after = 'cmp-nvim-lsp',
+  }
+
 end)
