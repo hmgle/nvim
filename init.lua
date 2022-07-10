cb545a8698d6ed7dfbe08b4cc8e3f153ed706037
mingang.he <dustgle@gmail.com>
2022-07-10 14:45:42 +0800

    Update

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+local ok, _ = pcall(require, 'impatient')
+
+if not ok then
+  vim.notify('impatient.nvim not installed', vim.log.levels.WARN)
+end
+
 require("basic")
 require("plugins")
 require("options")
