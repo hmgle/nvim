fe4a5924473b1b50bc4ffd0934af50c903c95c78
mingang.he <dustgle@gmail.com>
2023-02-06 14:45:47 +0800

    Update

--- lua/config/lsp/providers/null_ls.lua
+++ lua/config/lsp/providers/null_ls.lua
@@ -16,7 +16,7 @@ local sources = {
   }),
   -- null_ls.builtins.code_actions.gitsigns,
   null_ls.builtins.formatting.prettier.with({
-    filetypes = { "html", "css", "yaml", "markdown", "json" },
+    filetypes = { "html", "css", "shell", "markdown", "json" },
   }),
 }
 
