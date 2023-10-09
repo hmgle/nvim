5c679821d1a209fb2b19e0208207c17031650abc
mingang.he <dustgle@gmail.com>
2023-10-09 14:58:04 +0800

    Migrate to lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,48 +1,44 @@
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+return {
+  'nvim-lua/plenary.nvim',
 
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-
-  use {
+  {
     'preservim/tagbar',
     cmd = 'TagbarToggle',
     config = function ()
       vim.g.tagbar_silent = 1
     end
-  }
-  use 'godlygeek/tabular'
-  use 'tpope/vim-sleuth'
-  use 'tpope/vim-fugitive'
-  use {
+  },
+  'godlygeek/tabular',
+  'tpope/vim-sleuth',
+  'tpope/vim-fugitive',
+  {
     'kylechui/nvim-surround',
     config = function()
       require("nvim-surround").setup({
         -- Configuration here, or leave empty to use defaults
       })
     end
-  }
-  use 'tpope/vim-abolish'
-  use 'tpope/vim-repeat'
+  },
+  'tpope/vim-abolish',
+  'tpope/vim-repeat',
 
   -- nerdtree
-  use {
+  {
     'scrooloose/nerdtree',
     cmd = 'NERDTreeTabsToggle',
-  }
-  use 'jistr/vim-nerdtree-tabs'
+  },
+  'jistr/vim-nerdtree-tabs',
 
-
-  use {
+  {
     'phaazon/hop.nvim',
     branch = 'v1', -- optional but strongly recommended
     config = function()
       -- you can configure Hop the way you like here; see :h hop-config
       require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
     end
-  }
+  },
 
-  use {
+  {
     'ianva/vim-youdao-translater',
     config = function()
       local map = require('utils').map
@@ -50,12 +46,12 @@ return require('packer').startup(function()
       map('n', '<leader>ee', ':<C-u>Ydc<CR>')
       map('n', '<leader>yd', ':<C-u>Yde<CR>')
     end
-  }
-  use 'jremmen/vim-ripgrep'
-  use 'dyng/ctrlsf.vim'
-  use 'ap/vim-buftabline'
+  },
+  'jremmen/vim-ripgrep',
+  'dyng/ctrlsf.vim',
+  'ap/vim-buftabline',
 
-  use {
+  {
     "Yggdroot/LeaderF",
     config = function()
       vim.g.Lf_ShortcutF = '<c-p>'
@@ -63,124 +59,127 @@ return require('packer').startup(function()
       vim.g.Lf_PreviewInPopup = 0
       require('utils').map('', '<leader>lf', ':LeaderfFunction!<cr>')
     end,
-    run = ":LeaderfInstallCExtension"
-  }
+    build = ":LeaderfInstallCExtension"
+  },
 
-  use {
+  {
     'ray-x/go.nvim',
     config = function()
       require('go').setup()
     end
-  }
+  },
 
-  use {
+  {
     'nvim-treesitter/nvim-treesitter',
     config = function ()
       require('config.treesitter')
     end
-  }
+  },
 
-  use {
+  {
     'nvim-treesitter/nvim-treesitter-textobjects',
-  }
+  },
 
   -- autocompletion
-  use({
+  {
     'hrsh7th/nvim-cmp',
     commit = 'cfafe0a1ca8933f7b7968a287d39904156f2c57d',
     config = function()
       require('config.cmp')
     end,
-    requires = {
-      { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
-      { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' },
-      { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
-      { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
+    dependencies = {
+      { 'hrsh7th/cmp-buffer', dependencies = 'nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp', dependencies = 'nvim-cmp' },
+      { 'hrsh7th/cmp-path', dependencies = 'nvim-cmp' },
+      { 'saadparwaiz1/cmp_luasnip', dependencies = 'nvim-cmp' },
       {
         'L3MON4D3/LuaSnip',
         config = function ()
           require("luasnip.loaders.from_vscode").lazy_load()
         end,
-        requires = {
+        dependencies = {
           'rafamadriz/friendly-snippets',
         },
       },
     },
-  })
+  },
 
-  use({
+  {
     'neovim/nvim-lspconfig',
     config = function()
       require('config.lspconf')
     end,
-    requires = {
+    dependencies = {
       { 'williamboman/mason.nvim' },
       { 'williamboman/mason-lspconfig.nvim' },
       {
         'jose-elias-alvarez/null-ls.nvim',
-        requires = {{'neovim/nvim-lspconfig'}, {'nvim-lua/plenary.nvim'}},
+        dependencies = {{'neovim/nvim-lspconfig'}, {'nvim-lua/plenary.nvim'}},
         config = function()
           require('config.lsp.providers.null_ls')
         end,
-        after = 'nvim-lspconfig',
+        dependencies = 'nvim-lspconfig',
       },
       {
         'ray-x/lsp_signature.nvim',
         config = function()
           require('config.lsp-signature')
         end,
-        after = 'nvim-lspconfig',
+        dependencies = 'nvim-lspconfig',
       },
     },
-    after = 'cmp-nvim-lsp',
-  })
+    dependencies = 'cmp-nvim-lsp',
+  },
 
   -- great ui for lsp
-  use {
+  {
     'glepnir/lspsaga.nvim',
-    after = 'nvim-lspconfig',
+    dependencies = 'nvim-lspconfig',
     config = function() require('config.lspsaga') end,
-    disable = true,
-  }
+    enabled = false,
+  },
 
-  use {
+  {
     'nvim-telescope/telescope.nvim',
-    requires = {
+    dependencies = {
       'nvim-lua/popup.nvim',
-      'nvim-lua/plenary.nvim',
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        run = 'make',
-      },
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+      }
+      -- {
+      --   'nvim-telescope/telescope-fzf-native.nvim',
+      --   build = 'make',
+      -- },
     },
     config = function()
       require('config.telescope')
     end,
-    disable = false,
-  }
+    enabled = true,
+  },
 
-  use {
+  {
     "AckslD/nvim-neoclip.lua",
-    requires = {
+    dependencies = {
       {'nvim-telescope/telescope.nvim'},
     },
     config = function()
       require('config.neoclip')
     end,
-    disable = true,
-  }
+    enabled = false,
+  },
 
-  use {
+  {
     "gbprod/yanky.nvim",
-    requires = {
+    dependencies = {
       {'nvim-telescope/telescope.nvim'},
     },
     config = function()
       require('config.yanky')
     end,
-  }
+  },
 
-  use {
+  {
     'folke/tokyonight.nvim',
     config = function()
       vim.g.tokyonight_style = "day"
@@ -190,28 +189,28 @@ return require('packer').startup(function()
       vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
       vim.cmd[[colorscheme tokyonight]]
     end,
-    disable = true
-  }
+    enabled = false
+  },
 
-  use {
+  {
     'EdenEast/nightfox.nvim',
     -- config = function()
     --   vim.cmd("colorscheme dayfox")
     -- end,
-    disable = false
-  }
+    enabled = true
+  },
 
-  use {
+  {
     'savq/melange',
     config = function()
       vim.o.termguicolors = true
       vim.o.background = 'light'
       vim.cmd("colorscheme melange")
     end,
-    disable = true
-  }
+    enabled = false
+  },
 
-  use {
+  {
     'sainnhe/gruvbox-material',
     config = function()
       vim.o.termguicolors = true
@@ -221,26 +220,26 @@ return require('packer').startup(function()
       vim.g.gruvbox_material_current_word = 'bold'
       vim.cmd("colorscheme gruvbox-material")
     end
-  }
+  },
 
-  use {
+  {
     'RRethy/vim-illuminate',
     config = function ()
       vim.api.nvim_set_keymap('n', '<leader>n', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
       vim.api.nvim_set_keymap('n', '<leader>N', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
       -- vim.g.Illuminate_useDeprecated = 1
     end
-  }
+  },
 
-  use {
+  {
     'kosayoda/nvim-lightbulb',
-    requires = 'antoinemadec/FixCursorHold.nvim',
+    dependencies = 'antoinemadec/FixCursorHold.nvim',
     config = function()
       require('config.lightbulb')
     end,
-  }
+  },
 
-  use {
+  {
     'p00f/nvim-ts-rainbow',
     config = function ()
       require("nvim-treesitter.configs").setup {
@@ -254,14 +253,14 @@ return require('packer').startup(function()
         }
       }
     end
-  }
+  },
 
-  use {
+  {
     "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
-  }
+  },
 
-  use {
+  {
     'jeetsukumaran/vim-indentwise',
     config = function()
       local map = require('utils').map
@@ -270,9 +269,9 @@ return require('packer').startup(function()
       map('v', '<leader>d', '<Plug>(IndentWiseNextLesserIndent)')
       map('n', '<leader>d', '<Plug>(IndentWiseNextLesserIndent)')
     end
-  }
+  },
 
-  use {
+  {
     'numToStr/Comment.nvim',
     config = function()
       require('Comment').setup()
@@ -289,9 +288,9 @@ return require('packer').startup(function()
       vim.keymap.set('x', '<leader>cc', '<Plug>(comment_toggle_linewise_visual)')
       vim.keymap.set('x', '<leader>cb', '<Plug>(comment_toggle_blockwise_visual)')
     end
-  }
+  },
 
-  use {
+  {
     "folke/which-key.nvim",
     config = function()
       vim.o.timeout = true
@@ -305,15 +304,14 @@ return require('packer').startup(function()
         },
       }
     end
-  }
+  },
 
   -- clipboard over ssh through tmux
-  use {
+  {
     'ojroques/nvim-osc52',
     config = function()
       require('config.osc52')
     end,
-    disable = true,
-  }
-
-end)
+    enabled = false,
+  },
+}
