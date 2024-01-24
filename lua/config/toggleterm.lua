fb0e4924810300a8a362dbfd69e115e55fc3a7b1
mingang.he <dustgle@gmail.com>
2024-01-24 09:47:49 +0800

    Fix toggleterm keymap

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -6,10 +6,10 @@ require("toggleterm").setup {
 }
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-S><C-n>]], opts)
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-S><C-n><C-w>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
