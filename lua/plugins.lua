b9408d840fea6f4ba0c32b472346ba7295cf06ed
mingang.he <dustgle@gmail.com>
2024-05-08 23:07:55 +0800

    Update color

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -240,6 +240,9 @@ return {
       vim.g.gruvbox_material_current_word = 'bold'
       vim.g.gruvbox_material_foreground = 'original'
       vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_show_eob = 0
+      vim.g.gruvbox_material_dim_inactive_windows = 1
+      vim.g.gruvbox_material_ui_contrast = 'high'
       vim.cmd("colorscheme gruvbox-material")
     end
   },
