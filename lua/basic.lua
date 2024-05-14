a245ce840d327ea5e54403618fbb6dc021e4b83c
mingang.he <dustgle@gmail.com>
2024-05-14 20:29:30 +0800

    Update

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,5 +1,7 @@
 vim.g.encoding = "UTF-8"
 vim.o.fileencoding = 'utf-8'
+vim.o.fileencoding = 'utf-8'
+vim.g.python3_host_prog = '/usr/local/bin/python3'
 vim.opt.fileencodings = { 'utf-8', 'gbk', 'ucs-bom', 'cp936', 'gb18030' }
 
 vim.wo.number = true
