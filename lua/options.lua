6be971fcbb872acaca474f7c602a999323e2c6db
mingang.he <dustgle@gmail.com>
2024-05-19 20:28:01 +0800

    nerdtree => nvim-tree

--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,7 @@ local map = require('utils').map
 
 vim.g.mapleader = ','
 
-vim.g.NERDTreeWinSize = 24
-map('n', '<leader>tt', ':NERDTreeTabsToggle<CR>')
+map('n', '<leader>tt', ':NvimTreeToggle<CR>')
 
 map('', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>")
 map('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>")
