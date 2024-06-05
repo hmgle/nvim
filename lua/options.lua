6e0c9624c50c4c49cb6ae25e3a12ad316ede11bd
mingang.he <dustgle@gmail.com>
2024-06-05 13:42:56 +0800

    Fix Hop plugin keybindings: now only mapped in normal mode ('n')
    The previous configuration had issues where pressing 's', 'f', or 'F' would often fail when trying to update
    LuaSnip parameters.

--- lua/options.lua
+++ lua/options.lua
@@ -5,9 +5,9 @@ vim.g.mapleader = ','
 vim.g.NERDTreeWinSize = 24
 map('n', '<leader>tt', ':NERDTreeTabsToggle<CR>')
 
-map('', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>")
-map('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>")
-map('', 's', "<cmd>lua require'hop'.hint_char1({ current_line_only = false })<cr>")
+map('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>")
+map('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>")
+map('n', 's', "<cmd>lua require'hop'.hint_char1({ current_line_only = false })<cr>")
 
 vim.opt.completeopt = 'menu,menuone,noselect'
 vim.opt.mouse = 'a'
