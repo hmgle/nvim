0c184b91ca498ebbb3cea9bbd4fdbb4dd3916b86
mingang.he <dustgle@gmail.com>
2024-06-30 14:47:39 +0800

    Add lazydev.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -222,6 +222,29 @@ return {
     },
   },
 
+  {
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          { path = "luvit-meta/library", words = { "vim%.uv" } },
+        },
+      },
+    },
+    { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+    { -- optional completion source for require statements and module annotations
+      "hrsh7th/nvim-cmp",
+      opts = function(_, opts)
+        opts.sources = opts.sources or {}
+        table.insert(opts.sources, {
+          name = "lazydev",
+          group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+        })
+      end,
+    },
+  },
+
   -- great ui for lsp
   {
     'glepnir/lspsaga.nvim',
@@ -397,6 +420,7 @@ return {
         },
         triggers_blacklist = {
           n = { "o", "O", '"' },
+          v = { "j", "k", '"' },
         },
       }
     end
