e8b2fbc494af4178ebe6e85b1f3ee44509a9a8ca
mingang.he <dustgle@gmail.com>
2024-08-12 15:07:11 +0800

    performance: defer clipboard because xsel and pbcopy can be slow
    https://github.com/nvim-lua/kickstart.nvim/pull/1049
    https://www.reddit.com/r/neovim/comments/1e4je65/save_40_startuptime_for_free/

--- lua/options.lua
+++ lua/options.lua
@@ -2,40 +2,42 @@ local map = require('utils').map
 
 vim.g.mapleader = ','
 
-map('n', 'gt', "<cmd>bn<cr>")
-map('n', 'gT', "<cmd>bp<cr>")
+map('n', 'gt', '<cmd>bn<cr>')
+map('n', 'gT', '<cmd>bp<cr>')
 
 vim.opt.completeopt = 'menu,menuone,noselect'
 vim.opt.mouse = 'a'
-vim.opt.clipboard = 'unnamedplus' -- Sync with system clipboard
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus' -- Sync with system clipboard
+end)
 
 -- https://github.com/preservim/tagbar/wiki#zig
 vim.g.tagbar_type_zig = {
-    ctagstype = 'zig',
-    kinds = {
-        'f:functions',
-        's:structs',
-        'e:enums',
-        'u:unions',
-        'E:errors',
-    }
+  ctagstype = 'zig',
+  kinds = {
+    'f:functions',
+    's:structs',
+    'e:enums',
+    'u:unions',
+    'E:errors',
+  },
 }
 
 local function fcitx2en()
-    vim.fn.jobstart({ 'fcitx-remote' }, {
-        on_stdout = function(_, data, _)
-            if data and data[1] and data[1]:match("2") then -- 简体中文输入状态
-                vim.fn.jobstart('fcitx-remote -c') -- 切换到英文输入状态
-            end
-        end,
-    })
+  vim.fn.jobstart({ 'fcitx-remote' }, {
+    on_stdout = function(_, data, _)
+      if data and data[1] and data[1]:match '2' then -- 简体中文输入状态
+        vim.fn.jobstart 'fcitx-remote -c' -- 切换到英文输入状态
+      end
+    end,
+  })
 end
 
-if vim.fn.executable('fcitx-remote') == 1 then
-    vim.api.nvim_create_augroup("fcitx", {})
-    vim.api.nvim_create_autocmd("InsertLeave", {
-        group = "fcitx",
-        pattern = "*",
-        callback = fcitx2en,
-    })
+if vim.fn.executable 'fcitx-remote' == 1 then
+  vim.api.nvim_create_augroup('fcitx', {})
+  vim.api.nvim_create_autocmd('InsertLeave', {
+    group = 'fcitx',
+    pattern = '*',
+    callback = fcitx2en,
+  })
 end
