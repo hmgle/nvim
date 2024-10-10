86b72b4cb4031ce5a062ec2b39601229f4c397dd
mingang.he <dustgle@gmail.com>
2024-10-10 18:02:09 +0800

    Add dressing.nvim and nvim-notify

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -340,6 +340,7 @@ return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
       'nvim-lua/popup.nvim',
+      'rcarriga/nvim-notify',
       {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
@@ -582,6 +583,54 @@ return {
   },
 
   'LunarVim/bigfile.nvim',
+
+  {
+    'rcarriga/nvim-notify',
+    event = 'VeryLazy',
+    config = function()
+      local notify = require 'notify'
+      notify.setup { timeout = 2000 }
+      vim.notify = notify
+    end,
+  },
+
+  {
+    'stevearc/dressing.nvim',
+    event = 'VeryLazy',
+    config = function()
+      local utils = require 'utils'
+      local map, feedkeys = utils.map, utils.feedkeys
+
+      require('dressing').setup {
+        select = {
+          telescope = require('telescope.themes').get_dropdown(),
+        },
+        input = {
+          insert_only = false,
+          relative = 'editor',
+          default_prompt = ' ', -- Doesn't seem to work
+        },
+      }
+
+      vim.api.nvim_create_augroup('Dressing', {})
+      vim.api.nvim_create_autocmd('Filetype', {
+        pattern = 'DressingInput',
+        callback = function()
+          local input = require 'dressing.input'
+
+          if vim.g.grep_string_mode then
+            -- Enter input window in select mode
+            feedkeys('<Esc>V<C-g>', 'i')
+          end
+          map({ 'i', 's' }, '<C-j>', input.history_next, { buffer = true })
+          map({ 'i', 's' }, '<C-k>', input.history_prev, { buffer = true })
+          map({ 's', 'n' }, '<C-c>', input.close, { buffer = true })
+          map('s', '<CR>', input.confirm, { buffer = true })
+        end,
+        group = 'Dressing',
+      })
+    end,
+  },
 
   -- plantUML preview for .uml
   'aklt/plantuml-syntax',
