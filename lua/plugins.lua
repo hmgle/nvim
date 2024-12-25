417450c24d11d1ec939f7fdacbf2b95a6206c1cf
mingang.he <dustgle@gmail.com>
2024-12-25 09:36:44 +0800

    Update: blink.cmp Copilot kind_idx

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -293,6 +293,10 @@ return {
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
+
+        kind_icons = {
+          Copilot = '',
+        },
       },
 
       sources = {
@@ -302,6 +306,15 @@ return {
             module = 'blink-cmp-copilot',
             score_offset = 100,
             async = true,
+            transform_items = function(_, items)
+              local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
+              local kind_idx = #CompletionItemKind + 1
+              CompletionItemKind[kind_idx] = 'Copilot'
+              for _, item in ipairs(items) do
+                item.kind = kind_idx
+              end
+              return items
+            end,
           },
         },
         default = { 'lsp', 'path', 'luasnip', 'copilot', 'buffer' },
