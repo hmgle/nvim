cc96e126ee5eb02fe38079ff98f074239af1f8cc
mingang.he <dustgle@gmail.com>
2025-01-03 08:46:40 +0800

    Update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -899,6 +899,16 @@ return {
       },
       -- add any opts here
     },
+    keys = {
+      {
+        '<leader>v',
+        mode = { 'n', 'x', 'v' },
+        function()
+          require('avante').toggle()
+        end,
+        desc = 'Avante.Toggle',
+      },
+    },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = 'make',
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
