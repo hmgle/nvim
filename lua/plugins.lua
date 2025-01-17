17a68439ac592b27fc0c8245359323983cc53613
mingang.he <dustgle@gmail.com>
2025-01-17 09:03:51 +0800

    Removed the local directory specification for the aider.nvim plugin.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -941,7 +941,6 @@ return {
 
   {
     'aweis89/aider.nvim',
-    dir = '~/.local/share/nvim/lazy/aider.nvim',
     dependencies = {
       -- required for core functionality
       'akinsho/toggleterm.nvim',
