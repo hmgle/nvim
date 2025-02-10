253621135d3184a38099735924225e25701511bf
mingang.he <dustgle@gmail.com>
2025-02-10 16:20:54 +0800

    refactor: Update deepseek model from 'deepseek-r1' to 'deepseek-v3'

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -866,7 +866,7 @@ return {
           __inherited_from = 'openai',
           api_key_name = 'TX_DEEPSEEK_API_KEY',
           endpoint = 'https://api.lkeap.cloud.tencent.com/v1',
-          model = 'deepseek-r1',
+          model = 'deepseek-v3',
           temperature = 0,
         },
       },
