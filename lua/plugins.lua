754592fa52f45ea1835b03701c8aabfefc4aff72
mingang.he <dustgle@gmail.com>
2025-04-27 09:53:44 +0800

    chore: increase bigfile.nvim filesize threshold to 2 MiB

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -728,7 +728,7 @@ return {
     'LunarVim/bigfile.nvim',
     config = function()
       require('bigfile').setup {
-        filesize = 1, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+        filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
       }
     end,
   },
