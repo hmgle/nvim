c471bd6b0df3922e7134825ae49f409bd7aa7836
mingang.he <dustgle@gmail.com>
2026-04-14 17:10:20 +0800

    fix: unify lsp attach setup

--- lua/config/lspconf.lua
+++ lua/config/lspconf.lua
@@ -1,124 +1,150 @@
-vim.lsp.log.set_level('WARN')
+vim.lsp.log.set_level 'WARN'
 
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 
-require 'config.lsp.providers'
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if not client or client._supports_method_compat then
-      return
-    end
-    local mt = getmetatable(client)
-    local supports = mt and mt.supports_method
-    if type(supports) ~= 'function' then
-      return
-    end
-    client.supports_method = function(arg1, ...)
-      if arg1 == client then
-        return supports(client, ...)
-      else
-        return supports(client, arg1, ...)
-      end
-    end
-    client._supports_method_compat = true
-  end,
-})
-
-local mason_ok, masonlspconf = pcall(require, 'mason-lspconfig')
+local mason_ok, mason = pcall(require, 'mason')
 if not mason_ok then
   return
 end
 
+mason.setup {
+  ui = {
+    icons = {
+      package_installed = '✓',
+      package_pending = '➜',
+      package_uninstalled = '✗',
+    },
+  },
+}
+
+local masonlspconf_ok, masonlspconf = pcall(require, 'mason-lspconfig')
+if not masonlspconf_ok then
+  return
+end
+
+local function normalize_supports_method(client)
+  if client._supports_method_compat then
+    return
+  end
+
+  local mt = getmetatable(client)
+  local supports = mt and mt.supports_method
+  if type(supports) ~= 'function' then
+    return
+  end
+
+  client.supports_method = function(arg1, ...)
+    if arg1 == client then
+      return supports(client, ...)
+    else
+      return supports(client, arg1, ...)
+    end
+  end
+  client._supports_method_compat = true
+end
+
 local function enable_builtin_lsp_features(client, bufnr)
-  if client:supports_method('textDocument/codeLens') then
+  if client:supports_method 'textDocument/codeLens' then
     vim.lsp.codelens.enable(false, { bufnr = bufnr })
   end
 
-  if client:supports_method('textDocument/inlayHint') then
+  if client:supports_method 'textDocument/inlayHint' then
     vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
   end
 
-  if client:supports_method('textDocument/linkedEditingRange') then
+  if client:supports_method 'textDocument/linkedEditingRange' then
     vim.lsp.linked_editing_range.enable(true, { client_id = client.id })
   end
 end
 
--- do something on lsp attach
-local function on_attach(client, bufnr)
-  enable_builtin_lsp_features(client, bufnr)
-
+local function set_lsp_keymaps(bufnr)
   local function notify_toggle(feature, enabled)
     vim.notify(string.format('%s: %s', feature, enabled and 'ON' or 'OFF'), vim.log.levels.INFO)
   end
 
-  -- set mappings only in current buffer with lsp enabled
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
+  local opts = { buffer = bufnr, noremap = true, silent = true }
 
-  local opts = { noremap = true, silent = true }
-
-  buf_set_keymap('n', 'gh', '<cmd>Telescope lsp_document_symbols<CR>', opts)
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
-  buf_set_keymap('n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>Telescope lsp_references include_current_line=true<CR>', opts)
-  buf_set_keymap('n', '<leader>gf', "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
-  -- {{
-  -- using actions-preview
-  -- buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action({apply = true})<CR>', opts)
-  -- buf_set_keymap('v', '<leader>ca', '<cmd>vim.lsp.buf.range_code_action()<CR>', opts)
-  -- }}
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gc', '<cmd>Telescope lsp_incoming_calls<CR>', opts)
-  buf_set_keymap('n', '<leader>gc', '<cmd>lua vim.lsp.buf.incoming_calls()<CR>', opts)
-
-  buf_set_keymap('n', 'gl', '<cmd>Telescope diagnostics<CR>', opts)
+  vim.keymap.set('n', 'gh', '<cmd>Telescope lsp_document_symbols<CR>', opts)
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
+  vim.keymap.set('n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references include_current_line=true<CR>', opts)
+  vim.keymap.set('n', '<leader>gf', '<cmd>lua vim.lsp.buf.format { async = true }<CR>', opts)
+  vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.keymap.set('n', 'gc', '<cmd>Telescope lsp_incoming_calls<CR>', opts)
+  vim.keymap.set('n', '<leader>gc', '<cmd>lua vim.lsp.buf.incoming_calls()<CR>', opts)
+  vim.keymap.set('n', 'gl', '<cmd>Telescope diagnostics<CR>', opts)
   vim.keymap.set('n', '<leader>k', function()
-    vim.lsp.buf.hover({
+    vim.lsp.buf.hover {
       border = 'rounded',
-    })
+    }
   end, { buffer = bufnr, silent = true })
   vim.keymap.set('n', 'gs', function()
-    vim.lsp.buf.signature_help({
+    vim.lsp.buf.signature_help {
       border = 'rounded',
-    })
+    }
   end, { buffer = bufnr, silent = true })
   vim.keymap.set('n', '[d', function()
-    vim.diagnostic.jump({
+    vim.diagnostic.jump {
       count = -1,
-    })
+    }
   end, { buffer = bufnr, silent = true })
   vim.keymap.set('n', ']d', function()
-    vim.diagnostic.jump({
+    vim.diagnostic.jump {
       count = 1,
-    })
+    }
   end, { buffer = bufnr, silent = true })
 
   vim.keymap.set('n', '<leader>H', function()
-    local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+    local enabled = vim.lsp.inlay_hint.is_enabled { bufnr = bufnr }
     local next_enabled = not enabled
     vim.lsp.inlay_hint.enable(next_enabled, { bufnr = bufnr })
     notify_toggle('Inlay hints', next_enabled)
   end, { buffer = bufnr, silent = true, desc = 'Toggle inlay hints' })
   vim.keymap.set('n', '<leader>C', function()
-    local enabled = vim.lsp.codelens.is_enabled({ bufnr = bufnr })
+    local enabled = vim.lsp.codelens.is_enabled { bufnr = bufnr }
     local next_enabled = not enabled
     vim.lsp.codelens.enable(next_enabled, { bufnr = bufnr })
     notify_toggle('Code lens', next_enabled)
   end, { buffer = bufnr, silent = true, desc = 'Toggle code lens' })
-
-  local signature_ok, signature = pcall(require, 'lsp_signature')
-  if signature_ok then
-    signature.on_attach(client, bufnr)
-  end
 end
+
+local attach_group = vim.api.nvim_create_augroup('native-lsp-attach', { clear = true })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = attach_group,
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then
+      return
+    end
+
+    local bufnr = args.buf
+    normalize_supports_method(client)
+    enable_builtin_lsp_features(client, bufnr)
+
+    if not vim.b[bufnr].native_lsp_keymaps then
+      set_lsp_keymaps(bufnr)
+      vim.b[bufnr].native_lsp_keymaps = true
+    end
+
+    if vim.b[bufnr].native_lsp_signature then
+      return
+    end
+
+    if client:supports_method 'textDocument/signatureHelp' then
+      local signature_ok, signature = pcall(require, 'lsp_signature')
+      if signature_ok then
+        signature.on_attach(client, bufnr)
+        vim.b[bufnr].native_lsp_signature = true
+      end
+    end
+  end,
+})
 
 -- Setup LSP defaults first, then let mason-lspconfig enable installed servers.
 -- Relying on get_installed_servers() is fragile with newer mason-lspconfig
@@ -131,7 +157,6 @@ local function setup_lsp()
   end
 
   local default_options = {
-    on_attach = on_attach,
     capabilities = capabilities,
   }
   vim.lsp.config('*', default_options)
